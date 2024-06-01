4ccc296aab70c1f13cf497d84fc887967088a138
joelzil <jmo07oliveira@gmail.com>
2024-05-31 22:26:26 -0300

    Update script.py

--- script.py
+++ script.py
@@ -70,7 +70,7 @@ class InstaScript:
 
 
 if __name__ == '__main__':
-        Instagram_Spam_Bot = InstaScript('<Username>', '<Password>', '<victim_username>', <Number of messages to be spammed>)
-        Instagram_Spam_Bot.login()
-        Instagram_Spam_Bot.victim_profile()
-        Instagram_Spam_Bot.spamming()
+        Instagram_Spam_Bot = InstaScript('<milly.zpm>', '<903829010938201>', '<anonimy16002>', <2000>)
+        Instagram_Spam_Bot.login(anonimy16002)
+        Instagram_Spam_Bot.victim_profile(milly.zpm)
+        Instagram_Spam_Bot.spamming(milly.zpm)
